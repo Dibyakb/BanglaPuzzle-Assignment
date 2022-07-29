dbe3de083ed46cd85f106bb9395b6d19707a32f2
Dibya Kanti Basak <57316711+Dibyakb@users.noreply.github.com>
2022-07-29 21:28:01 +0600

    Add files via upload
    I have installed tailwind in a node environment and since the node module folder is too large, I have compressed it so that it will be easier to upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dibya.html"],
+  theme: {
+    extend: {
+      colors:{
+        blue:{
+          950: '#00001a'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
